15603303afbfe13b71f94a43f07eae03777a9581
trangwvw <nttt4304@gmail.com>
2024-03-06 14:45:29 +0700

    sua all do loi thanh toan

--- app/src/main/java/Activity/DangNhapActivity.java
+++ app/src/main/java/Activity/DangNhapActivity.java
@@ -1,4 +1,4 @@
-package com.example.doan_mobileapp;
+package Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +8,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.doan_mobileapp.R;
 
 public class DangNhapActivity extends AppCompatActivity {
     EditText editUser, editSigninPass;
