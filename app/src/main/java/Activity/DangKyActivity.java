9944e199a9fb63ed8863732d1af616677d42411f
Phamdinhchuong1001 <chuongpham10012004@gmail.com>
2024-03-24 17:48:05 +0700

    dangky,dangnhap

--- app/src/main/java/Activity/DangKyActivity.java
+++ app/src/main/java/Activity/DangKyActivity.java
@@ -1,65 +1,85 @@
 package Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.doan_mobileapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DangKyActivity extends AppCompatActivity {
-    EditText editHoten,editEmail,editSignupPass,editRePass;
-    Button btnSignup;
-    TextView txtSignin;
-    ImageView imgSUFB,imgSUGG;
+    EditText edtHoTen,edtEmail,edtPass,edtRePass;
+    Button btnSignUp;
+    TextView txtSignIn;
+    ImageView ivSIFB,ivSIGG;
+    FirebaseAuth firebaseAuth= FirebaseAuth.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_ky);
 
-        addControl();
-        addBtnEvent();
+        edtHoTen=findViewById(R.id.edtHoTen);
+        edtEmail=findViewById(R.id.edtEmail);
+        edtPass=findViewById(R.id.edtPass);
+        edtRePass=findViewById(R.id.edtRePass);
+        btnSignUp=findViewById(R.id.btnSignUp);
+        txtSignIn=findViewById(R.id.txtSignIn);
 
-    }
-
-    private void addBtnEvent() {
-        btnSignup.setOnClickListener(new View.OnClickListener() {
+        txtSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String hoten = editHoten.getText().toString().trim();
-                String email = editEmail.getText().toString().trim();
-                String password = editSignupPass.getText().toString().trim();
-                String repass = editRePass.getText().toString().trim();
-
-                Intent getInformation = new Intent(DangKyActivity.this, DangNhapActivity.class);
-                getInformation.putExtra("hoten",hoten);
-                getInformation.putExtra("email",email);
-                getInformation.putExtra("password",password);
-                startActivity(getInformation);
+                Intent intent=new Intent(DangKyActivity.this,DangNhapActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
-        txtSignin.setOnClickListener(new View.OnClickListener() {
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent openSignin = new Intent(DangKyActivity.this, DangNhapActivity.class);
-                startActivity(openSignin);
+                String email,password;
+                email = String.valueOf(edtEmail.getText());
+                password = String.valueOf(edtPass.getText());
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(DangKyActivity.this,"Enter Email",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(DangKyActivity.this,"Enter Password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(DangKyActivity.this,"Đăng ký thành công",Toast.LENGTH_SHORT).show();
+                            Intent intent=new Intent(DangKyActivity.this,DangNhapActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(DangKyActivity.this,"Đăng ký thất bại",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
-    }
 
-    public void addControl(){
-        editHoten = findViewById(R.id.editHoten);
-        editSignupPass = findViewById(R.id.editSignupPass);
-        editEmail = findViewById(R.id.editEmail);
-        editRePass = findViewById(R.id.editRePass);
-        txtSignin = findViewById(R.id.txtSignin);
-        btnSignup = findViewById(R.id.btnSignup);
-        imgSUFB = findViewById(R.id.imgSUFB);
-        imgSUGG = findViewById(R.id.imgSUGG);
     }
 }
+
--- app/src/main/java/Activity/DangNhapActivity.java
+++ app/src/main/java/Activity/DangNhapActivity.java
@@ -2,63 +2,94 @@ package Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.doan_mobileapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import Activity.DangKyActivity;
-
 public class DangNhapActivity extends AppCompatActivity {
-    EditText editSigninUser, editSigninPass;
-    TextView txtForgtoPass, txtSignup;
-    Button btnSignin;
-    ImageView imgSIFB,imgSIGG;
+    EditText edtSignIpUser,edtSignInPass;
+    TextView txtForgotPass,txtSignUp;
+    Button btnSignIn;
+    ImageView ivSIFB,ivSIGG;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_nhap);
 
-        addControl();
-        addBtnEvent();
-        Event();
-    }
 
-    private void Event() {
-        Intent getInformation = getIntent();
-        String email = getInformation.getStringExtra("email");
-        editSigninUser.setText(email);
-    }
+        edtSignIpUser=findViewById(R.id.edtSignInUser);
+        edtSignInPass=findViewById(R.id.edtSignInPass);
+        txtForgotPass=findViewById(R.id.txtForgotPass);
+        txtSignUp=findViewById(R.id.txtSignUp);
+        btnSignIn=findViewById(R.id.btnSignIn);
+        ivSIFB=findViewById(R.id.ivSIFB);
+        ivSIGG=findViewById(R.id.ivSIGG);
 
-    private void addBtnEvent() {
-        txtSignup.setOnClickListener(new View.OnClickListener() {
+        txtSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent openSignup = new Intent(DangNhapActivity.this, DangKyActivity.class);
-                startActivity(openSignup);
+                Intent intent=new Intent(DangNhapActivity.this,DangKyActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
-        btnSignin.setOnClickListener(new View.OnClickListener() {
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent openMain = new Intent(DangNhapActivity.this, MainActivity.class);
-                startActivity(openMain);
+                Intent intent=new Intent(DangNhapActivity.this, DangKyActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
-    }
 
-    private void addControl() {
-        editSigninUser = findViewById(R.id.editSigninUser);
-        editSigninPass = findViewById(R.id.editSigninPass);
-        txtForgtoPass = findViewById(R.id.txtForgtoPass);
-        txtSignup = findViewById(R.id.txtSignup);
-        btnSignin = findViewById(R.id.btnSignin);
-        imgSIFB = findViewById(R.id.imgSIFB);
-        imgSIGG = findViewById(R.id.imgSIGG);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email,password;
+                email =String.valueOf(edtSignIpUser.getText());
+                password=String.valueOf(edtSignInPass.getText());
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(DangNhapActivity.this,"Enter Email",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(DangNhapActivity.this,"Enter Password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(DangNhapActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                            Intent intent=new Intent(DangNhapActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(DangNhapActivity.this,"Authentication Failed",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
+
