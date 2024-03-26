e658f4c2947d46d7e79c032ed9f281974a1c9843
Nhựt Tân <119086524+Nguyennhuttan2004@users.noreply.github.com>
2024-03-26 14:15:58 +0700

    logout and fix dndk

---
+++ app/src/main/java/Activity/SplashActivity.java
@@ -0,0 +1,39 @@
+package Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.doan_mobileapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                nextAtivity();
+            }
+        },2000);
+    }
+    private void nextAtivity() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user == null){
+            // chua login
+            Intent intent =  new Intent(this, DangNhapActivity.class);
+            startActivity(intent);
+        }else{
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        finish();
+    }
+}
